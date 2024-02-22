948996fd47a7dd8f86a13af5ded3b2f5361d7da9
R2bEEaton <scgryan@gmail.com>
2024-02-22 11:38:07 -0500

    updated with persistent storage

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # HeadStones
 
 My third plugin. For 1.20.4. Made on 2/22/2024.
+
+When players die without keepInventory, their player head will be place at the death location.
+When this head is broken, it will drop all items and xp as it would if the player died.
+This head is immune to water and lava 
--- src/main/java/cc/spea/headstones/HeadStones.java
+++ src/main/java/cc/spea/headstones/HeadStones.java
@@ -10,19 +10,24 @@ import org.bukkit.entity.ExperienceOrb;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockFromToEvent;
+import org.bukkit.event.block.BlockPhysicsEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class HeadStones extends JavaPlugin implements Listener {
-    public HashMap<String, List<ItemStack>> graveItems = new HashMap<>();
-    public HashMap<String, Integer> graveExp = new HashMap<>();
-
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
@@ -39,12 +44,27 @@ public class HeadStones extends JavaPlugin implements Listener {
 
         Location loc = event.getEntity().getLocation();
         Block skullBlock = loc.getBlock();
+
+        // Go up to find a valid spot
+        while (skullBlock.getType() != Material.AIR) {
+            skullBlock = loc.add(0, 1, 0).getBlock();
+        }
+        // Go down to the surface
+        while (skullBlock.getType() == Material.AIR) {
+            skullBlock = loc.add(0, -1, 0).getBlock();
+        }
+        skullBlock = loc.add(0, 1, 0).getBlock();
+
         skullBlock.setType(Material.PLAYER_HEAD);
         Skull skull = (Skull) skullBlock.getState();
 
-        String key = event.getEntity().getUniqueId() + " " + skullBlock.getLocation();
-        graveItems.put(key, List.copyOf(event.getDrops()));
-        graveExp.put(key, event.getDroppedExp());
+        List<String> inventory = new ArrayList<>();
+        for (ItemStack is : event.getDrops()) {
+            inventory.add(itemStackToBase64(is));
+        }
+
+        skull.getPersistentDataContainer().set(new NamespacedKey(this, "items"), PersistentDataType.LIST.strings(), inventory);
+        skull.getPersistentDataContainer().set(new NamespacedKey(this, "xp"), PersistentDataType.INTEGER, event.getDroppedExp());
 
         event.getDrops().clear();
         event.setDroppedExp(0);
@@ -55,19 +75,84 @@ public class HeadStones extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
-        Location loc = event.getBlock().getLocation();
-        Skull skull = (Skull) event.getBlock().getState();
-        String key = skull.getOwningPlayer().getUniqueId() + " " + loc;
-        if (graveItems.containsKey(key)) {
-            event.setDropItems(false);
-            for (ItemStack is : graveItems.get(key)) {
-                loc.getWorld().dropItemNaturally(loc, is);
+        if (event.getBlock().getType() == Material.PLAYER_HEAD) breakPlayerHead(event.getBlock());
+    }
+
+    @EventHandler
+    public void onBlockFromTo(BlockFromToEvent event) {
+        if (event.getToBlock().getType() == Material.PLAYER_HEAD) {
+            if (((Skull) event.getToBlock().getState()).getPersistentDataContainer().has(new NamespacedKey(this, "items"))) {
+                event.setCancelled(true);
             }
-            graveItems.remove(key);
         }
-        if (graveExp.containsKey(key)) {
-            loc.getWorld().spawn(loc, ExperienceOrb.class).setExperience(graveExp.get(key));
-            graveExp.remove(key);
+    }
+
+    public void breakPlayerHead(Block block) {
+        Location loc = block.getLocation();
+        Skull skull = (Skull) block.getState();
+
+        List<String> items = skull.getPersistentDataContainer().get(new NamespacedKey(this, "items"), PersistentDataType.LIST.strings());
+        if (items != null) {
+            for (String it : items) {
+                try {
+                    ItemStack itemStack = itemStackFromBase64(it);
+                    loc.getWorld().dropItemNaturally(loc, itemStack);
+                } catch (IOException e) {
+                }
+            }
+        }
+
+        Integer xp = skull.getPersistentDataContainer().get(new NamespacedKey(this, "xp"), PersistentDataType.INTEGER);
+        if (xp != null) {
+            loc.getWorld().spawn(loc, ExperienceOrb.class).setExperience(xp);
+        }
+    }
+
+
+
+    /**
+     * Gets one {@link ItemStack} from Base64 string.
+     * <a href="https://gist.github.com/graywolf336/8153678?permalink_comment_id=4536153#gistcomment-4536153">Source</a>
+     * @param data Base64 string to convert to {@link ItemStack}.
+     * @return {@link ItemStack} created from the Base64 string.
+     * @throws IOException
+     */
+    public static ItemStack itemStackFromBase64(String data) throws IOException {
+        try {
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decode(data));
+            BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
+            ItemStack item;
+
+            // Read the serialized inventory
+            item = (ItemStack) dataInput.readObject();
+
+            dataInput.close();
+            return item;
+        } catch (ClassNotFoundException e) {
+            throw new IOException("Unable to decode class type.", e);
+        }
+    }
+
+    /**
+     * A method to serialize one {@link ItemStack} to Base64 String.
+     * <a href="https://gist.github.com/graywolf336/8153678?permalink_comment_id=4536153#gistcomment-4536153">Source/a>
+     * @param item to turn into a Base64 String.
+     * @return Base64 string of the item.
+     * @throws IllegalStateException
+     */
+    public static String itemStackToBase64(ItemStack item) throws IllegalStateException {
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
+
+            // Save every element
+            dataOutput.writeObject(item);
+
+            // Serialize that array
+            dataOutput.close();
+            return new String(Base64Coder.encode(outputStream.toByteArray()));
+        } catch (Exception e) {
+            throw new IllegalStateException("Unable to save item stacks.", e);
         }
     }
 }
